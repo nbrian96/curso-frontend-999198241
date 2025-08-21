da34fac8e031c4db242326c960ae57a206dbcc53
Nelson Avila <nelson.avila@redtecnologica.org>
2025-08-21 19:58:04 -0300

    ejercicios

---
+++ material-educativo/clase16/index.js
@@ -0,0 +1,16 @@
+const ejercicio01 = document.getElementById('ejercicio01');
+
+ejercicio01.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const numero = parseInt(ejercicio01.querySelector('#numero').value, 10);
+  let resultado = "TABLA DE MULTIPLICAR " + numero + "<br>";
+
+  for (let i = 1; i < 11; i++) {
+    const multi = numero * i;
+    resultado += `${numero} x ${i} = ${multi} <br>`;
+  }
+
+
+  document.getElementById('resultado01').innerHTML = resultado;
+});
